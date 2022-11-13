d4042d8fe6509501eff268d3d6fca79ad6d66c3d
Mark Scott <43500665+marsscotia@users.noreply.github.com>
2022-11-13 14:21:06 +0000

    - Add note regarding injecting service provider

--- App.xaml.cs
+++ App.xaml.cs
@@ -2,7 +2,14 @@
 
 public partial class App : Application
 {
-	public App()
+	/// <summary>
+    /// Note to future self: We don't seem to be able to 
+    /// inject the MainPage type into the constructor here,
+    /// so we inject the service provider and resolve the type
+    /// manually.
+    /// </summary>
+    /// <param name="serviceProvider"></param>
+    public App(IServiceProvider serviceProvider)
 	{
 		InitializeComponent();
 
